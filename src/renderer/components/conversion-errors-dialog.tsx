4e5dec29e203b20970f2906321d8a02ff232e1c7
Andrew <30775786+andrewk604@users.noreply.github.com>
2026-01-25 19:01:03 +0100

    Fixed UI in error dialog

--- src/renderer/components/conversion-errors-dialog.tsx
+++ src/renderer/components/conversion-errors-dialog.tsx
@@ -23,7 +23,7 @@ export function ConversionErrorsDialog({ open, onOpenChange, failedFiles, totalP
     return (
         <Dialog open={open} onOpenChange={onOpenChange}>
             <DialogContent className="h-full max-h-105 w-full justify-between gap-0 p-0" showCloseButton={false}>
-                <div className="flex h-full w-full flex-col gap-5 p-5">
+                <div className="flex w-full flex-1 flex-col gap-4 p-4">
                     <div className="flex flex-col items-start">
                         <DialogTitle className="text-popover-foreground text-lg font-bold">Conversion issues</DialogTitle>
                         <DialogDescription className="text-muted-softer text-sm">
@@ -31,7 +31,7 @@ export function ConversionErrorsDialog({ open, onOpenChange, failedFiles, totalP
                         </DialogDescription>
                     </div>
 
-                    <div className="max-h-76 overflow-y-auto pr-2">
+                    <div className="max-h-64 overflow-y-auto pr-2">
                         <div className="flex flex-col gap-3">
                             {failedFiles.map((file) => (
                                 <div key={file.id} className="border-border-muted flex flex-col gap-1 pb-3 last:border-b-0">
@@ -53,18 +53,17 @@ export function ConversionErrorsDialog({ open, onOpenChange, failedFiles, totalP
 
                 <DialogFooter className="items-end justify-center">
                     <div className="flex gap-3">
+                        <Button variant="secondary" onClick={handleCopyAllDetails}>
+                            Copy all details
+                        </Button>
                         <Button
                             onClick={async () => {
                                 await window.App.window.openDebugLog();
                             }}
                             type="button"
                             variant={'default'}
-                            className="text-popover-foreground/80 w-fit brightness-90"
                         >
                             Open debug log
-                        </Button>
-                        <Button variant="default" onClick={handleCopyAllDetails}>
-                            Copy all details
                         </Button>
                     </div>
                 </DialogFooter>
